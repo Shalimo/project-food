a937a440f5d5e80fcbede3acd1d8dfb7a87893fa
Nikita Shalimo <MegaFrostBall@gmail.com>
2021-08-22 23:43:51 +0300

    menu cards

--- js/script.js
+++ js/script.js
@@ -143,5 +143,72 @@ window.addEventListener('DOMContentLoaded' , () => {
         }
     }
     
+     //clases
+
+     class MenuCard {
+        constructor(src, title, descr, price, parent, ...classes) {
+            this.src = src;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.course = 27;
+            this.parent = document.querySelector(parent);
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.course;
+        }
+
+        structureOfCard() {
+            const element = document.createElement('div');
+            
+            if (this.classes.length === 0) {
+                this.class = 'menu__item';
+                element.classList.add(this.class);
+            } else {
+                this.classes.forEach(className => {
+                    element.classList.add(className);
+                });
+            }
+
+            element.innerHTML = `
+                            <img src=${this.src} alt="vegy">
+                            <h3 class="menu__item-subtitle">${this.title}</h3>
+                            <div class="menu__item-descr">${this.descr}</div>
+                            <div class="menu__item-divider"></div>
+                            <div class="menu__item-price">
+                                <div class="menu__item-cost">Цена:</div>
+                                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                            </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        22,
+        '.menu .container'
+    ).structureOfCard();
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        22,
+        '.menu .container',
+    ).structureOfCard();
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        22,
+        '.menu .container',
+    ).structureOfCard();
 
 });
