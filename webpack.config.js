347c9677ec6dfb6d4852776a9425215d021bbf93
Nikita Shalimo <MegaFrostBall@gmail.com>
2021-09-07 23:06:12 +0300

    usage babel and core.js in project

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
@@ -13,5 +13,22 @@ module.exports = {
 
   devtool: "source-map",
 
-  module: {}
+  module: { // какие модули используем
+    rules: [ // правила
+      {
+        test: /\.m?js$/, // находим js файлы с помощью регулярных выражений
+        exclude: /(node_modules|bower_components)/, // какие файлы мы должны исключить
+        use: { // описываем как и что будем использовать
+          loader: 'babel-loader', // loader - технология, которая связывает вебпак с бэбелем
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
